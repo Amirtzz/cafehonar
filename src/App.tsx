564b0a6a92ea07308ad2b12d4f37dedaf7a2c6a9
Amirtzz <amirbbxplayer@gmail.com>
2024-11-25 09:53:03 -0800

    change 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import omletImage from "./Images/Foods/Omlet.jpg";
 import cheeseBurgerImage from "./Images/Foods/Cheese Burger.jpg";
@@ -21,113 +21,156 @@ import "./App.css";
 import FoodItem from "./components/FoodItem/FoodItem";
 
 function App() {
-  return (
-    <div className="App">
-      <main>
-        <FoodItem
-          foodname="املت"
-          price="80000 تومان"
-          image={omletImage}
-          count={-1}
-        />
-        <FoodItem
-          foodname="چیزبرگر"
-          price="200000 تومان"
-          image={cheeseBurgerImage}
-          count={18}
-        />
-        <FoodItem
-          foodname="چیپس و پنیر"
-          price="160000 تومان"
-          image={chipsoPanirImage}
-          count={23}
-        />
-        <FoodItem
-          foodname="فیله سوخاری"
-          price="210000 تومان"
-          image={fileSokhariImage}
-          count={17}
-        />
-        <FoodItem
-          foodname="فرنچ فرایز"
-          price="110000 تومان"
-          image={frenchFriesImage}
-          count={13}
-        />
-        <FoodItem
-          foodname="هات داگ"
-          price="150000 تومان"
-          image={hotdogImage}
-          count={17}
-        />
-        <FoodItem
-          foodname="همبرگر"
-          price="180000 تومان"
-          image={humburgerImage}
-          count={31}
-        />
-        <FoodItem
-          foodname="پنینی مرغ با سس پستو"
-          price="200000 تومان"
-          image={paniniMorghBaSosPastuImage}
-          count={7}
-        />
-        <FoodItem
-          foodname="پاستا آلفردو"
-          price="180000 تومان"
-          image={pastaAlferedoImage}
-          count={22}
-        />
-        <FoodItem
-          foodname="پاستا چیکن پستو"
-          price="180000 تومان"
-          image={pastaChickenPastuImage}
-          count={21}
-        />
-        <FoodItem
-          foodname="رویال هات داگ تنوری"
-          price="170000 تومان"
-          image={royalHotdogTanuriImage}
-          count={18}
-        />
-        <FoodItem
-          foodname="رویال مرغ سوخاری"
-          price="220000 تومان"
-          image={royalMorghSokhariImage}
-          count={15}
-        />
-        <FoodItem
-          foodname="سالاد سزار"
-          price="200000 تومان"
-          image={saladeSezarImage}
-          count={25}
-        />
-        <FoodItem
-          foodname="سیب زمینی با سس قارچ"
-          price="150000 تومان"
-          image={sibzaminiBaSoseGharchImage}
-          count={31}
-        />
-        <FoodItem
-          foodname="سیب زمینی با سوسیس و سس پنیر"
-          price="180000 تومان"
-          image={sibzaminiBaSosisoSosePanirImage}
-          count={27}
-        />
-        <FoodItem
-          foodname="استیک مرغ"
-          price="200000 تومان"
-          image={steakeMorghImage}
-          count={34}
-        />
-        <FoodItem
-          foodname="تست مخصوص"
-          price="175000 تومان"
-          image={tosteMakhsusImage}
-          count={23}
-        />
-      </main>
-    </div>
-  );
+  const App: React.FC = () => {
+    const [changeMode, setChangeMode] = useState(0);
+    const [mode, setMode] = useState<"increase" | "decrease">("increase");
+    useEffect(() => {
+      const handleKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "i") {
+          setMode("increase");
+        } else if (event.key === "d") {
+          setMode("decrease");
+        }
+      };
+      window.addEventListener("keydown", handleKeyPress);
+      return () => {
+        window.removeEventListener("keydown", handleKeyPress);
+      };
+    }, []);
+    const handleClick = () => {
+      if (mode === "increase") {
+        setChangeMode((prevchangemode) => prevchangemode + 1);
+      }
+      if (mode === "decrease") {
+        setChangeMode((prevchangemode) => prevchangemode - 1);
+      }
+      console.log(changeMode);
+    };
+    return (
+      <div className="App">
+        <main>
+          <FoodItem
+            foodname="املت"
+            price="80000 تومان"
+            image={omletImage}
+            count={34}
+            mode=""
+          />
+          <FoodItem
+            foodname="چیزبرگر"
+            price="200000 تومان"
+            image={cheeseBurgerImage}
+            count={18}
+            mode=""
+          />
+          <FoodItem
+            foodname="چیپس و پنیر"
+            price="160000 تومان"
+            image={chipsoPanirImage}
+            count={23}
+            mode=""
+          />
+          <FoodItem
+            foodname="فیله سوخاری"
+            price="210000 تومان"
+            image={fileSokhariImage}
+            count={17}
+            mode=""
+          />
+          <FoodItem
+            foodname="فرنچ فرایز"
+            price="110000 تومان"
+            image={frenchFriesImage}
+            count={13}
+            mode=""
+          />
+          <FoodItem
+            foodname="هات داگ"
+            price="150000 تومان"
+            image={hotdogImage}
+            count={17}
+            mode=""
+          />
+          <FoodItem
+            foodname="همبرگر"
+            price="180000 تومان"
+            image={humburgerImage}
+            count={31}
+            mode=""
+          />
+          <FoodItem
+            foodname="پنینی مرغ با سس پستو"
+            price="200000 تومان"
+            image={paniniMorghBaSosPastuImage}
+            count={7}
+            mode=""
+          />
+          <FoodItem
+            foodname="پاستا آلفردو"
+            price="180000 تومان"
+            image={pastaAlferedoImage}
+            count={22}
+            mode=""
+          />
+          <FoodItem
+            foodname="پاستا چیکن پستو"
+            price="180000 تومان"
+            image={pastaChickenPastuImage}
+            count={21}
+            mode=""
+          />
+          <FoodItem
+            foodname="رویال هات داگ تنوری"
+            price="170000 تومان"
+            image={royalHotdogTanuriImage}
+            count={18}
+            mode=""
+          />
+          <FoodItem
+            foodname="رویال مرغ سوخاری"
+            price="220000 تومان"
+            image={royalMorghSokhariImage}
+            count={15}
+            mode=""
+          />
+          <FoodItem
+            foodname="سالاد سزار"
+            price="200000 تومان"
+            image={saladeSezarImage}
+            count={25}
+            mode=""
+          />
+          <FoodItem
+            foodname="سیب زمینی با سس قارچ"
+            price="150000 تومان"
+            image={sibzaminiBaSoseGharchImage}
+            count={31}
+            mode=""
+          />
+          <FoodItem
+            foodname="سیب زمینی با سوسیس و سس پنیر"
+            price="180000 تومان"
+            image={sibzaminiBaSosisoSosePanirImage}
+            count={27}
+            mode=""
+          />
+          <FoodItem
+            foodname="استیک مرغ"
+            price="200000 تومان"
+            image={steakeMorghImage}
+            count={34}
+            mode=""
+          />
+          <FoodItem
+            foodname="تست مخصوص"
+            price="175000 تومان"
+            image={tosteMakhsusImage}
+            count={23}
+            mode=""
+          />
+        </main>
+      </div>
+    );
+  };
 }
 export default App;
--- src/components/FoodItem/FoodItem.tsx
+++ src/components/FoodItem/FoodItem.tsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
+
 function FoodItem(attrs: {
   foodname: string;
   image: any;
   price: string;
   count: number;
+  mode: string;
 }) {
+  const [changeMode, setchangeMode] = useState(attrs.count);
+  const [mode, setMode] = useState<"increase" | "decrease">("increase");
   let foodAvailabilityClass = "available";
   let CountSpan = <span>{attrs.count}</span>;
   if (attrs.count == -1) {
     foodAvailabilityClass = "unavailable";
     CountSpan = <span>&infin;</span>;
   }
+
+  const handleClick = () => {
+    if (mode === "increase") {
+      setchangeMode((prevchangemode) => prevchangemode + 1);
+    }
+    if (mode === "decrease") {
+      setchangeMode((prevchangemode) => prevchangemode - 1);
+    }
+    console.log(changeMode);
+  };
   return (
     <section className="section-menu">
-      <article className={"menu-item " + foodAvailabilityClass}>
+      <article
+        onClick={handleClick}
+        className={"menu-item " + foodAvailabilityClass}
+      >
         <img src={attrs.image} />
         <h3>{attrs.foodname}</h3>
         <br />
         <br />
         <br />
-        {CountSpan}
+        <span>{changeMode}</span>
         <div dir="rtl" className="price">
           <strong>{attrs.price}</strong>
         </div>
